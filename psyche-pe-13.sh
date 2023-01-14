cf40f9787215d61f0ba0da886b8d68a41f03dcdd
stuartore <stuart.dr@qq.com>
2023-01-14 19:44:59 +0800

    test: no update

--- psyche-pe-13.sh
+++ psyche-pe-13.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This file is generated for Xiaomi 12X (psyche)
 
-cd $HOME/aosp-setup/android/pe
-
 # make new mkdir
 mkdir -p device/xiaomi vendor/xiaomi kernel/xiaomi
 # device specific dir
@@ -11,11 +9,11 @@ mkdir -p vendor/xiaomi-firmware
 # pull source for device/ vendor/ kernel
 git clone https://github.com/pixelexperience/hardware_xiaomi -b thirteen hardware/xiaomi
 git clone https://github.com/stuartore/device_xiaomi_psyche -b thirteen device/xiaomi/psyche
-git clone https://github.com/stuartore/device_xiaomi_sm8250-common -b thirteen-rebase device/xiaomi/sm8250-common
+git clone https://github.com/stuartore/device_xiaomi_sm8250-common -b thirteen device/xiaomi/sm8250-common
 git clone https://gitlab.com/stuartore/android_vendor_xiaomi_psyche -b arrow-13.0 vendor/xiaomi/psyche
 git clone https://gitlab.com/stuartore/vendor_xiaomi_sm8250-common.git -b thirteen vendor/xiaomi/sm8250-common
 git clone https://gitlab.com/stuartore/vendor_xiaomi_psyche-firmware -b thirteen vendor/xiaomi-firmware/psyche
-git clone https://github.com/UtsavBalar1231/kernel_xiaomi_sm8250 -b android13-stable kernel/xiaomi/sm8250
+git clone --depth=1 https://github.com/stuartore/kernel_xiaomi_sm8250 -b thirteen kernel/xiaomi/sm8250
 
 # other
 echo 'include $(call all-subdir-makefiles)' > vendor/xiaomi-firmware/Android.mk
