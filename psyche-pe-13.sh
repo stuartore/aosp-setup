2c946f2f15bb450c2d4e3097b2adef4c97e10248
stuartore <stuart.dr@qq.com>
2023-02-06 16:17:57 +0800

    adjust: use linage hardware/xiaomi

--- psyche-pe-13.sh
+++ psyche-pe-13.sh
@@ -7,7 +7,7 @@ mkdir -p device/xiaomi vendor/xiaomi kernel/xiaomi
 mkdir -p vendor/xiaomi-firmware
 
 # pull source for device/ vendor/ kernel
-git clone https://github.com/pixelexperience/hardware_xiaomi -b thirteen hardware/xiaomi
+git clone https://github.com//LineageOS/android_hardware_xiaomi -b thirteen hardware/xiaomi
 git clone https://github.com/stuartore/device_xiaomi_psyche -b thirteen device/xiaomi/psyche
 git clone https://github.com/stuartore/device_xiaomi_sm8250-common -b thirteen device/xiaomi/sm8250-common
 git clone https://gitlab.com/stuartore/android_vendor_xiaomi_psyche -b arrow-13.0 vendor/xiaomi/psyche
