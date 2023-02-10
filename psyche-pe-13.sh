dc770f633e89c8f0f10c4e6f036949a6b78cc4f5
stuartore <stuart.dr@qq.com>
2023-02-10 19:27:55 +0800

    build: pass: update script

--- psyche-pe-13.sh
+++ psyche-pe-13.sh
@@ -3,17 +3,35 @@
 
 # make new mkdir
 mkdir -p device/xiaomi vendor/xiaomi kernel/xiaomi
-# device specific dir
-#mkdir -p vendor/xiaomi-firmware
 
-# pull source for device/ vendor/ kernel
-git clone https://github.com//LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-git clone https://github.com/stuartore/device_xiaomi_psyche -b thirteen-test device/xiaomi/psyche
-#git clone https://github.com/stuartore/device_xiaomi_sm8250-common -b thirteen device/xiaomi/sm8250-common
-git clone https://gitlab.com/stuartore/android_vendor_xiaomi_psyche -b thirteen vendor/xiaomi/psyche
-#git clone https://gitlab.com/stuartore/vendor_xiaomi_sm8250-common.git -b thirteen vendor/xiaomi/sm8250-common
-#git clone https://gitlab.com/stuartore/vendor_xiaomi_psyche-firmware -b thirteen vendor/xiaomi-firmware/psyche
-git clone --depth=1 https://github.com/stuartore/kernel_xiaomi_sm8250 -b thirteen kernel/xiaomi/sm8250
+psyche_use_common_deps(){
+	# NOT AVAIABLE 
 
-# other
-# echo 'include $(call all-subdir-makefiles)' > vendor/xiaomi-firmware/Android.mk
+	# pull source for device/ vendor/ kernel
+	# device specific dir
+	mkdir -p vendor/xiaomi-firmware
+	
+	# pull source for device/ vendor/ kernel
+	git clone https://github.com//LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+	git clone https://github.com/stuartore/device_xiaomi_psyche -b thirteen-test device/xiaomi/psyche
+	git clone https://github.com/stuartore/device_xiaomi_sm8250-common -b thirteen device/xiaomi/sm8250-common
+	git clone https://gitlab.com/stuartore/android_vendor_xiaomi_psyche -b thirteen vendor/xiaomi/psyche
+	git clone https://gitlab.com/stuartore/vendor_xiaomi_sm8250-common.git -b thirteen vendor/xiaomi/sm8250-common
+	git clone https://gitlab.com/stuartore/vendor_xiaomi_psyche-firmware -b thirteen vendor/xiaomi-firmware/psyche
+	git clone --depth=1 https://github.com/stuartore/kernel_xiaomi_sm8250 -b thirteen kernel/xiaomi/sm8250
+	
+	# other
+	echo 'include $(call all-subdir-makefiles)' > vendor/xiaomi-firmware/Android.mk
+}
+
+psyche_deps(){
+	# pull source for device/ vendor/ kernel
+
+	git clone https://github.com//LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+	git clone https://github.com/stuartore/android_device_xiaomi_psyche -b thirteen device/xiaomi/psyche
+	git clone https://gitlab.com/stuartore/android_vendor_xiaomi_psyche -b thirteen vendor/xiaomi/psyche
+	#git clone https://gitlab.com/stuartore/vendor_xiaomi_psyche-firmware -b thirteen vendor/xiaomi-firmware/psyche
+	git clone --depth=1 https://github.com/stuartore/kernel_xiaomi_sm8250 -b thirteen kernel/xiaomi/sm8250
+}
+
+psyche_deps
