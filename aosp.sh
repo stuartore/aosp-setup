a0de895737256ba81ffae530cd847d09f5aae4a2
stuartore <stuart.dr@outlook.com>
2024-11-30 14:25:36 +0800

    aosp-setup: auto build: add Android 15 auto build ability for Xiaomi 12X (psyche)) #1 [2/2]

--- aosp.sh
+++ aosp.sh
@@ -1374,7 +1374,7 @@ auto_build(){
 		export rom_spec_str=$rom_spec_str
  
 		# build command
-  		if [[ $(grep 'revision="android-14' .repo/manifests/default.xml) ]];then
+  		if [[ $(grep 'revision="android-15' .repo/manifests/default.xml) || $(grep 'revision="android-14' .repo/manifests/default.xml) ]];then
 			case $rom_spec_str in
 				"rising")
 					build_rom_cmd="riseup ${build_device} userdebug && rise fb"
@@ -1395,9 +1395,8 @@ auto_build(){
 					build_rom_cmd="lunch ${rom_spec_str}_${build_device}-userdebug && mka bacon"
 					;;
 			esac
-
 		# default for android 13
-    	else
+	    	else
 			local rom_spec_str=$rom_spec_str
 			case $rom_spec_str in
 				"evolution" | "pixys" | "xd")
