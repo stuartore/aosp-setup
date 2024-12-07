4eb4782dcedc3673df27b7455e9018770c8f7c07
stuartore <stuart.dr@outlook.com>
2024-12-07 22:27:28 +0800

    aosp-setup: deps: archlinux: add pyelftools package

--- aosp.sh
+++ aosp.sh
@@ -579,7 +579,7 @@ Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch \
 		sudo pacman-key --lsign-key "farseerfc@archlinux.org"
 		sudo pacman -Sy --noconfirm archlinuxcn-keyring
 	fi
-	sudo pacman -Syy --noconfirm --needed git git-lfs multilib-devel fontconfig ttf-droid yay ccache make yay patch pkg-config maven gradle 
+	sudo pacman -Syy --noconfirm --needed git git-lfs multilib-devel fontconfig ttf-droid yay ccache make yay patch pkg-config maven gradle python-pyelftools
 	packages=(ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel python-pip python-setuptools p7zip)
 	yay -Sy --noconfirm --norebuild --noredownload ${packages[@]}
 	sudo pacman -S --noconfirm --needed android-tools # android-udev
