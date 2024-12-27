b62b5b85e65bc83d376a4083da786cf2ebbb87ec
stuartore <stuart.dr@outlook.com>
2024-12-27 09:56:07 +0800

    aosp-setup: deps: add package for Fedora

--- aosp.sh
+++ aosp.sh
@@ -590,7 +590,7 @@ Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch \
 
 fedora_deps(){
 	sudo dnf install -y \
-		android-tools autoconf213 bison bzip2 ccache clang curl flex gawk gcc-c++ git git-lfs p7zip glibc-devel glibc-static libstdc++-static libX11-devel make mesa-libGL-devel ncurses-devel openssl patch zlib-devel ncurses-devel.i686 readline-devel.i686 zlib-devel.i686 libX11-devel.i686 mesa-libGL-devel.i686 glibc-devel.i686 libstdc++.i686 libXrandr.i686 zip perl-Digest-SHA python2 python3-pyelftools wget lzop openssl-devel java-1.8.0-openjdk-devel ImageMagick schedtool lzip vboot-utils vim ccache libxcrypt-compat
+		android-tools autoconf213 bison bzip2 ccache clang curl flex gawk gcc-c++ git git-lfs p7zip glibc-devel glibc-static libstdc++-static libX11-devel make mesa-libGL-devel ncurses-devel openssl patch zlib-devel ncurses-devel.i686 readline-devel.i686 zlib-devel.i686 libX11-devel.i686 mesa-libGL-devel.i686 glibc-devel.i686 libstdc++.i686 libXrandr.i686 zip perl-Digest-SHA python2 python3-pyelftools wget lzop openssl openssl-devel java-1.8.0-openjdk-devel ImageMagick schedtool lzip vboot-utils vim ccache libxcrypt-compat
 
 	# The package libncurses5 is not available, so we need to hack our way by symlinking the required library.
 	sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
